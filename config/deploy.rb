cd2624501d0b24bda66f0620e97064d1c6899b42
Leandro <leandro.costantini@gmail.com>
2014-11-24 23:56:13 +0000

    minor changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'basic_rails_app'
+set :repo_url, 'git@github.com:leandroalemao/basic_rails_app.git'
+set :rbenv_ruby, '2.1.5'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
